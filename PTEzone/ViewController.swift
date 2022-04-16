7f4e59b05d56f86e92bb803ceac4974877054a84
andy <tuananhasp@gmail.com>
2022-04-16 20:26:20 +1000

    main screen

---
+++ PTEzone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PTEzone
+//
+//  Created by Owner on 9/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
