0564667c093be498b173832b4737c9e04ef608d6
andy <tuananhasp@gmail.com>
2022-09-22 11:52:26 +1000

    grammar check

--- PTEzone/Listening/SummariseSpoken/LisSumAnalyse.swift
+++ PTEzone/Listening/SummariseSpoken/LisSumAnalyse.swift
@@ -14,9 +14,25 @@ class LisSumAnalyse: UIView {
     @IBOutlet weak var progGrammar: KDCircularProgress!
     @IBOutlet weak var progForm: KDCircularProgress!
     
+    @IBOutlet weak var expandGrammarHeight: NSLayoutConstraint!
+    @IBOutlet weak var expandGrammarView: UIView!
     
     @IBAction func CloseView(_ sender: Any) {
         self.removeFromSuperview()
     }
     
+    @IBAction func expandGrammar(_ sender: Any) {
+//        let textView = UITextView(frame: CGRect(x: 50, y: 0, width: 250.0, height: 100.0))
+//        textView.contentInsetAdjustmentBehavior = .automatic
+//        textView.center = self.view.center
+//        textView.textAlignment = NSTextAlignment.justified
+//        textView.textColor = UIColor.blue
+//        textView.backgroundColor = UIColor.lightGray
+        
+        if(expandGrammarHeight.constant < 200){
+            expandGrammarHeight.constant = 200
+        }else{
+            expandGrammarHeight.constant = 50
+        }
+    }
 }
