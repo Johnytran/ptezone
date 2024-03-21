acd446577c64bdadfb4aeeabeea2ce9b89e1c4ed
Tuan Anh <testaccount@192-168-1-112.tpgi.com.au>
2024-03-21 11:50:01 +1100

    doing collection view

--- PTEzone/PTEzone/Listening/SummariseSpoken/LisSumSpoken.swift
+++ PTEzone/PTEzone/Listening/SummariseSpoken/LisSumSpoken.swift
@@ -7,6 +7,22 @@
 
 import UIKit
 
-class LisSumSpoken: UIViewController{
+class LisSumSpoken: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
+    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+        
+        
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! LisSumSokenCollectionCell
+    
+        
+        return cell
+    }
+    
     
 }
