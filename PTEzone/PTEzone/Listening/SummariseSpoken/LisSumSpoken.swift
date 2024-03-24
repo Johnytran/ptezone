ea0f85bc0699757dfef692aff000682444779f2f
Tuan Anh <testaccount@192-168-1-112.tpgi.com.au>
2024-03-24 20:30:59 +1100

    update launchscreen

--- PTEzone/PTEzone/Listening/SummariseSpoken/LisSumSpoken.swift
+++ PTEzone/PTEzone/Listening/SummariseSpoken/LisSumSpoken.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class LisSumSpoken: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
-    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    let reuseIdentifier = "liscell" // also enter this string as the cell identifier in the storyboard
     var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
         
-        
+    @IBOutlet weak var LisCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        self.LisCollectionView.dataSource = self;
+        self.LisCollectionView.delegate = self;
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1     //return number of sections in collection view
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.items.count;
     }
